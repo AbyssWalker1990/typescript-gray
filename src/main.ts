7f8808418a4037b686d8735b6ec2af8482d1bf04
Vova <bordiian.v@gmail.com>
2023-07-11 21:00:40 +0300

    learned more about generics

--- src/main.ts
+++ src/main.ts
@@ -1,95 +1,155 @@
-// Index Signatures
+const echo = <T>(arg: T): T => arg
 
-// interface TransactionObj {
-//   readonly [index: string]: number
-// }
-
-interface TransactionObj {
-  readonly [index: string]: number
-  Pizza: number,
-  Books: number,
-  Job: number
+const isObj = <T>(arg: T): boolean => {
+  return (typeof arg === 'object'
+    && !Array.isArray(arg)
+    && arg !== null)
 }
 
-const todaysTransations: TransactionObj = {
-  Pizza: -10,
-  Books: -5,
-  Job: 50
-}
+console.log(isObj(true))
+console.log(isObj('John'))
+console.log(isObj([1, 2, 3]))
+console.log(isObj({ name: 'John' }))
+console.log(isObj(null))
 
-console.log(todaysTransations.Pizza)
-console.log(todaysTransations['Pizza'])
-
-let prop: string = 'Pizza'
-console.log(todaysTransations[prop])
-
-const todaysNet = (transactions: TransactionObj): number => {
-  let total = 0
-  for (const transaction in transactions) {
-    total += transactions[transaction]
+const isTrue = <T>(arg: T): { arg: T, is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false }
   }
-  return total
+  if (isObj(arg) && !Object.keys(arg as unknown as Record<string, unknown>).length) {
+    return { arg, is: false }
+  }
+  return { arg, is: !!arg }
+}
+console.log('//////////////////////')
+console.log(isTrue(false))
+console.log(isTrue(0))
+console.log(isTrue(true))
+console.log(isTrue(1))
+console.log(isTrue('Vova'))
+console.log(isTrue(''))
+console.log(isTrue(null))
+console.log(isTrue(undefined))
+console.log(isTrue({}))
+console.log(isTrue({ name: 'Vova' }))
+console.log(isTrue([]))
+console.log(isTrue([1, 2, 3]))
+console.log(isTrue(NaN))
+console.log(isTrue(-0))
+
+///////////////////////////////
+interface BoolCheck<T> {
+  value: T,
+  is: boolean
 }
 
-console.log(todaysNet(todaysTransations))
 
-// todaysTransations.Pizza = 40 // not allowed becuse readonly
-
-console.log(todaysTransations['Dave'])
-
-
-////////////////////////////////
-
-interface Student {
-  // [key: string]: string | number | number[] | undefined
-  name: string
-  GPA: number
-  classes?: number[]
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { value: arg, is: false }
+  }
+  if (isObj(arg) && !Object.keys(arg as unknown as Record<string, unknown>).length) {
+    return { value: arg, is: false }
+  }
+  return { value: arg, is: !!arg }
 }
 
-const student: Student = {
-  name: 'Doug',
-  GPA: 3.5,
-  classes: [100, 200]
+//////////////////////
+
+interface HasID {
+  id: number
 }
 
-// console.log(student.test) // Still can access property that doesnt exist
+const processUser = <T extends HasID>(user: T): T => {
 
-// for (const key in student) {
-//   console.log(`${key}: ${student[key]}`) // works only with index signature
-// }
-
-for (const key in student) {
-  console.log(`${key}: ${student[key as keyof Student]}`) // works even WITHOUT index signature
+  return user
 }
 
-Object.keys(student).map(key => {
-  console.log(student[key as keyof typeof student]) // Also works
-})
+console.log(processUser({ id: 1, name: 'Vova' }))
+// console.log(processUser({name: 'Vova'})) // Dont work
 
-const logStudentKey = (student: Student, key: keyof Student): void => {
-  console.log(`${key}: ${student[key]}`)
+
+const getUsersProperty = <T extends HasID, K extends keyof T>(users: T[], key: K): T[K][] => {
+  return users.map(user => user[key])
 }
 
-logStudentKey(student, 'GPA')
-logStudentKey(student, 'name')
+const usersArray = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+]
 
-//////////////////////////////////////
+console.log(getUsersProperty(usersArray, 'email'))
+console.log(getUsersProperty(usersArray, 'username'))
 
-// interface Incomes {
-//   [key: string]: number
-// }
+/////////////
 
-type Streams = 'salary' | 'bonus' | 'sidehustle'
+class StateObject<T> {
+  private data: T
 
-type Incomes = Record<Streams, number>
+  constructor(value: T) {
+    this.data = value
+  }
 
-const monthlyIncomes: Incomes = {
-  salary: 500,
-  bonus: 100,
-  sidehustle: 250
+  get state(): T {
+    return this.data
+  }
+
+  set state(value: T) {
+    this.data = value
+  }
 }
 
-for (const revenue in monthlyIncomes) {
-  console.log(monthlyIncomes[revenue as keyof Incomes])
-}
+const store = new StateObject('John')
+console.log(store.state)
+store.state = 'Vova'
+// store.state = 12 // Dont work because typescript new that it must be string now
+
+
+// We can cpecify type manually
+const myState = new StateObject<(string | number | boolean)[]>([15])
+myState.state = (['Vova', 22, true])
+console.log(myState.state)
